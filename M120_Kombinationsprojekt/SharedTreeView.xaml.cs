71bf6de365e1e92492e60c66f62f0190cd852655
matthias <matthias_planzer@sluz.ch>
2019-05-31 11:26:34 +0200

    Order Dialog + Shard View

---
+++ M120_Kombinationsprojekt/SharedTreeView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M120_Kombinationsprojekt
+{
+    /// <summary>
+    /// Interaktionslogik für SharedTreeView.xaml
+    /// </summary>
+    public partial class SharedTreeView : Page
+    {
+        public SharedTreeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
